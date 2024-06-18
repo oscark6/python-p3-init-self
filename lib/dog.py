0b67c0dea512d14437d04ba7e1e6447be94a3023
oscark6 <oscar.kimani1@student.moringaschool.com>
2024-06-18 07:33:31 +0300

    done

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
+# Define the Dog class
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+# Create instances of Dog with different combinations of arguments
+dog1 = Dog("Fido")
+dog2 = Dog("Buddy", "Golden Retriever")
+
+# Print out the name and breed of each dog instance
+print(f"Dog 1: {dog1.name}, {dog1.breed}")
+print(f"Dog 2: {dog2.name}, {dog2.breed}")
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
+      # Define the Person class
 class Person:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+# Create instances of Person with different names
+person1 = Person("Daniel")
+person2 = Person("Bob")
+
+# Print out the name of each person instance
+print(f"Person 1: {person1.name}")
+print(f"Person 2: {person2.name}")
